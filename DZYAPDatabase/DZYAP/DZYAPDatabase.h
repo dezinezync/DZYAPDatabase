4b1fe4b136bccba34a04206f85c6d63f69abef57
Nikhil N <nikhil@dezinezync.com>
2014-02-22 20:40:31 +0530

    [ADD] Initial Commit

---
+++ DZYAPDatabase/DZYAP/DZYAPDatabase.h
@@ -0,0 +1,55 @@
+//
+//  DZYAPDatabase.h
+//  DZYAPDatabase
+//
+//  Created by Nikhil Nigade on 16/02/14.
+//  Copyright (c) 2014 Nikhil Nigade. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+#import "YapDatabase.h"
+
+typedef void(^DZYAPGetBlock)(BOOL finished,id obj);
+typedef void(^DZYAPDelBlock)(BOOL finished);
+
+@interface DZYAPDatabase : NSObject
+
+@property (nonatomic, strong) YapDatabase *db;
+@property (nonatomic, strong) NSMutableDictionary *connections;
+
+#pragma mark - SET
+// Set a value, will replace value if it already exists
++(void)set:(id)value key:(NSString *)key;
+// Set a value, will not replace an existing object
++(void)setNX:(id)value key:(NSString *)key;
+
++(void)setBG:(id)value key:(NSString *)key;
++(void)setNXBG:(id)value key:(NSString *)key;
+
++(void)set:(id)value key:(NSString *)key collection:(NSString *)collection;
++(void)setNX:(id)value key:(NSString *)key collection:(NSString *)collection;
++(void)setBG:(id)value key:(NSString *)key collection:(NSString *)collection;
++(void)setNXBG:(id)value key:(NSString *)key collection:(NSString *)collection;
+
+
+#pragma mark - GET
++(id)get:(NSString *)key;
+
++(void)getBG:(NSString *)key complete:(DZYAPGetBlock)complete;
+
++(id)get:(NSString *)key fromCollection:(NSString *)collection;
++(void)getBG:(NSString *)key fromCollection:(NSString *)collection complete:(DZYAPGetBlock)complete;
+
+#pragma mark - DEL
++(void)del:(NSString *)key;
++(void)delBG:(NSString *)key;
+
++(void)del:(NSString *)key fromCollection:(NSString *)collection;
++(void)delBG:(NSString *)key fromCollection:(NSString *)collection;
+
+/*
+ *  The following will always be called on the main thread.
+ *  Unless otherwise mentioned, the methods can be called across any thread
+ */
+
+@end
---
+++ DZYAPDatabase/DZYAP/DZYAPDatabase.m
@@ -0,0 +1,227 @@
+//
+//  DZYAPDatabase.m
+//  DZYAPDatabase
+//
+//  Created by Nikhil Nigade on 16/02/14.
+//  Copyright (c) 2014 Nikhil Nigade. All rights reserved.
+//
+
+static NSString const *kMainConnection = @"main";
+static NSString const *kBackgroundConnection = @"background";
+static NSString *kDefCollection = @"dzyap";
+static NSString *dbName = @"DZYAPDB.sqlite";
+
+static NSUInteger kCacheLimit = 5000;
+
+#import "DZYAPDatabase.h"
+
+@implementation DZYAPDatabase
+
+- (instancetype)init
+{
+    if(self = [super init])
+    {
+     
+        NSURL *docsPath = [[[NSFileManager defaultManager] URLsForDirectory:NSDocumentDirectory inDomains:NSUserDomainMask] lastObject];
+        
+        NSString *databasePath = [docsPath.absoluteString stringByAppendingPathComponent:dbName];
+        self.db = [[YapDatabase alloc] initWithPath:databasePath];
+        self.db.defaultObjectCacheLimit = kCacheLimit;
+        self.db.defaultMetadataCacheLimit = kCacheLimit;
+        
+        self.connections = @{}.mutableCopy;
+        
+        //Get and set our main connection
+        [self.connections setObject:[self.db newConnection] forKey:kMainConnection];
+        
+        //Get and set our background connection
+        dispatch_sync(dispatch_get_global_queue(DISPATCH_QUEUE_PRIORITY_BACKGROUND, 0), ^{
+            
+            @synchronized(self.connections)
+            {
+                [self.connections setObject:[self.db newConnection] forKey:kBackgroundConnection];
+            }
+            
+        });
+        
+    }
+    return self;
+}
+
++ (instancetype)shared
+{
+    static dispatch_once_t onceToken;
+    static DZYAPDatabase *DZDB;
+    dispatch_once(&onceToken, ^{
+        DZDB = [[DZYAPDatabase alloc] init];
+    });
+    
+    return DZDB;
+}
+
+#pragma mark - SET in Default Collection
++(void)set:(id)value key:(NSString *)key
+{
+    [DZYAPDatabase set:value key:key collection:kDefCollection];
+}
+
++(void)setNX:(id)value key:(NSString *)key
+{
+    [DZYAPDatabase setNX:value key:key collection:kDefCollection];
+}
+
++(void)setBG:(id)value key:(NSString *)key
+{
+    [DZYAPDatabase setBG:value key:key collection:kDefCollection];
+}
+
++(void)setNXBG:(id)value key:(NSString *)key
+{
+    [DZYAPDatabase setNXBG:value key:key collection:kDefCollection];
+}
+
+#pragma mark - SET in named Collection
++(void)set:(id)value key:(NSString *)key collection:(NSString *)collection
+{
+    YapDatabaseConnection *connection =  (YapDatabaseConnection *)[[DZYAPDatabase shared].connections objectForKey:kMainConnection];
+    
+    [connection asyncReadWriteWithBlock:^(YapDatabaseReadWriteTransaction *transaction) {
+        
+        [transaction setObject:value forKey:key inCollection:collection];
+        
+    }];
+}
+
++(void)setNX:(id)value key:(NSString *)key collection:(NSString *)collection
+{
+    YapDatabaseConnection *connection =  (YapDatabaseConnection *)[[DZYAPDatabase shared].connections objectForKey:kMainConnection];
+    
+    [connection readWithBlock:^(YapDatabaseReadTransaction *transaction) {
+        
+        if([transaction objectForKey:key inCollection:collection] == nil)
+        {
+            [connection asyncReadWriteWithBlock:^(YapDatabaseReadWriteTransaction *wtransaction) {
+                [wtransaction setObject:value forKey:key inCollection:collection];
+            }];
+        }
+        
+    }];
+}
+
++(void)setBG:(id)value key:(NSString *)key collection:(NSString *)collection
+{
+    YapDatabaseConnection *connection =  (YapDatabaseConnection *)[[DZYAPDatabase shared].connections objectForKey:kBackgroundConnection];
+    
+    [connection asyncReadWithBlock:^(YapDatabaseReadTransaction *transaction) {
+        
+        if([transaction objectForKey:key inCollection:collection] == nil)
+        {
+            [connection asyncReadWriteWithBlock:^(YapDatabaseReadWriteTransaction *wtransaction) {
+                [wtransaction setObject:value forKey:key inCollection:collection];
+            }];
+        }
+        
+    }];
+}
+
++(void)setNXBG:(id)value key:(NSString *)key collection:(NSString *)collection
+{
+    YapDatabaseConnection *connection =  (YapDatabaseConnection *)[[DZYAPDatabase shared].connections objectForKey:kBackgroundConnection];
+    __block id ourOBJ;
+    
+    [connection asyncReadWithBlock:^(YapDatabaseReadTransaction *transaction) {
+        
+        [transaction getObject:&ourOBJ metadata:nil forKey:key inCollection:collection];
+        
+    } completionBlock:^{
+        
+        if(ourOBJ == nil)
+        {
+            [connection asyncReadWriteWithBlock:^(YapDatabaseReadWriteTransaction *wtransaction) {
+                [wtransaction setObject:value forKey:key inCollection:collection];
+            }];
+        }
+        
+    }];
+}
+
+#pragma mark - GET from default Collection
+
++(id)get:(NSString *)key
+{
+    return [DZYAPDatabase get:key fromCollection:kDefCollection];
+}
+
++(void)getBG:(NSString *)key complete:(DZYAPGetBlock)complete
+{
+    [DZYAPDatabase getBG:key fromCollection:kDefCollection complete:complete];
+}
+
+#pragma mark - GET from named Collection
++(id)get:(NSString *)key fromCollection:(NSString *)collection
+{
+    __block id ourOBJ;
+    
+    YapDatabaseConnection *connection =  (YapDatabaseConnection *)[[DZYAPDatabase shared].connections objectForKey:kMainConnection];
+    
+    [connection readWithBlock:^(YapDatabaseReadTransaction *transaction) {
+        
+        ourOBJ = [transaction objectForKey:key inCollection:collection];
+        
+    }];
+    
+    return ourOBJ;
+}
+
++(void)getBG:(NSString *)key fromCollection:(NSString *)collection complete:(DZYAPGetBlock)complete
+{
+    
+    YapDatabaseConnection *connection = (YapDatabaseConnection *)[[DZYAPDatabase shared].connections objectForKey:kBackgroundConnection];
+    
+    [connection readWithBlock:^(YapDatabaseReadTransaction *transaction) {
+        
+        id ourOBJ = [transaction objectForKey:key inCollection:collection];
+        
+        if(ourOBJ == nil) complete(NO, nil);
+        else complete(YES, ourOBJ);
+        
+    }];
+
+}
+
+#pragma mark - DEL from default Collection
+
++(void)del:(NSString *)key
+{
+    [DZYAPDatabase del:key fromCollection:kDefCollection];
+}
+
++(void)delBG:(NSString *)key
+{
+    [DZYAPDatabase delBG:key fromCollection:kDefCollection];
+}
+
+#pragma mark - DEL from named Collection
++(void)del:(NSString *)key fromCollection:(NSString *)collection
+{
+    YapDatabaseConnection *connection =  (YapDatabaseConnection *)[[DZYAPDatabase shared].connections objectForKey:kMainConnection];
+    
+    [connection readWriteWithBlock:^(YapDatabaseReadWriteTransaction *transaction) {
+        
+        [transaction removeObjectForKey:key inCollection:collection];
+        
+    }];
+}
+
++(void)delBG:(NSString *)key fromCollection:(NSString *)collection
+{
+    YapDatabaseConnection *connection =  (YapDatabaseConnection *)[[DZYAPDatabase shared].connections objectForKey:kBackgroundConnection];
+    
+    [connection readWriteWithBlock:^(YapDatabaseReadWriteTransaction *transaction) {
+        
+        [transaction removeObjectForKey:key inCollection:collection];
+        
+    }];
+}
+
+@end
